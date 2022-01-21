b834ef904211260e16b84175e7f28f835b43ca35
imanepicduck <65570612+imanepicduck@users.noreply.github.com>
2022-01-21 13:46:23 -0500

    Add files via upload

---
+++ 760mmHg.py
@@ -0,0 +1,10 @@
+class atm:
+	def __init__(self, CashWithdrawl, BalanceEnquiry):
+		self.CashWithdrawl = "how much money do you want to spend it on crap you don't need?"
+       
+def print_money(self):
+		print(self.CashWithdrawl)
+
+obj = atm()
+
+obj.print_money()
